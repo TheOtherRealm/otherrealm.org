c8d2d7eecac9efa5d9982cbbc855be037b8fdbd1
Aaron E-J <the@otherrealm.org>
2020-02-06 12:02:56 -0500

    Working on wysiwyg pluggin

--- wp-content/plugins/wysiwyg/wysiwyg.php
+++ wp-content/plugins/wysiwyg/wysiwyg.php
@@ -1,6 +1,11 @@
 <?php
 /**
- * Plugin Name: YOUR PLUGIN NAME
+ * Plugin Name: wysiwyg
+ * Version: 1.0.0
+ * Author: Aaron E-J
+ * Author URI: https://otherrealm.org
+ * License:           GNU Affero General Public License Version 3
+ * License URI:       https://www.gnu.org/licenses/agpl-3.0.html
  */
 /*
   Created		: Feb 2, 2020, 12:38:19 PM
@@ -13,6 +18,9 @@
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU Affero General Public License for more details: <http://www.gnu.org/licenses/>.
+  GNU Affero General Public License for more details: <https://www.gnu.org/licenses/agpl-3.0.html>.
  */
-?>
+register_activation_hook( __FILE__, 'setup_wysiwyg' );
+register_deactivation_hook( __FILE__, 'takedown_wysiwyg' );
+register_uninstall_hook(__FILE__, 'uninstall_wysiwyg');
+plugins_url( 'myscript.js', _FILE_ );
--- wp-content/themes/otherrealm/functions.php
+++ wp-content/themes/otherrealm/functions.php
@@ -182,6 +182,8 @@ function otherrealm_scripts() {
 	wp_enqueue_script('otherrealm-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true);
 	wp_enqueue_script('otherrealm-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true);
 	wp_enqueue_script('otherrealmFunctions', get_template_directory_uri() . '/js/otherrealmFunctions.js', array('jquery'), '20200127', true);
+	wp_enqueue_script('tinymce', get_template_directory_uri() . '/js/tinymce/js/tinymce/tinymce.min.js', array('jquery'), '20200205', true);
+	wp_enqueue_script('tinymcejquery', get_template_directory_uri() . '/js/tinymce/js/tinymce/jquery.tinymce.min.js', array('jquery'), '20200205', true);
 	if (is_singular() && comments_open() && get_option('thread_comments')) {
 		wp_enqueue_script('comment-reply');
 	}
